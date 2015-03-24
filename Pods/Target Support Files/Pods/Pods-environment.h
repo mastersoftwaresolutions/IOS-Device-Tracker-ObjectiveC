547399240de19b89818c291fa1d96dbca9318ab8
jrkiosdev <mss.gaganpreet@gmail.com>
2015-03-24 10:30:02 +0530

    initial commit

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ALSystemUtilities
+#define COCOAPODS_POD_AVAILABLE_ALSystemUtilities
+#define COCOAPODS_VERSION_MAJOR_ALSystemUtilities 1
+#define COCOAPODS_VERSION_MINOR_ALSystemUtilities 0
+#define COCOAPODS_VERSION_PATCH_ALSystemUtilities 0
+
+// ALSystemUtilities/no-arc
+#define COCOAPODS_POD_AVAILABLE_ALSystemUtilities_no_arc
+#define COCOAPODS_VERSION_MAJOR_ALSystemUtilities_no_arc 1
+#define COCOAPODS_VERSION_MINOR_ALSystemUtilities_no_arc 0
+#define COCOAPODS_VERSION_PATCH_ALSystemUtilities_no_arc 0
+
